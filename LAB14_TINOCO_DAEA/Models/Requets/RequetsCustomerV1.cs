c8eb501f0b609e2b7dda78f03ba04eb122301aea
Jacko Isaias Tinoco Flores <jacko.tinoco@tecsup.edu.pe>
2024-06-22 00:07:29 -0500

    Agregar archivos de proyecto.

---
+++ LAB14_TINOCO_DAEA/Models/Requets/RequetsCustomerV1.cs
@@ -0,0 +1,9 @@
+﻿namespace LAB14_TINOCO_DAEA.Models.Request
+{
+    public class RequetsCustomerV1
+    {
+        public string FirstName { get; set; }
+        public string LasttName { get; set; }
+        public string DocumentNumber { get; set; }
+    }
+}
